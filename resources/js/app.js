72de1d1109d8ff55f899c88e2c00b138a966fdc4
SumejaV <sumiveseli@gmail.com>
2025-03-14 00:32:15 +0100

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,11 @@
-
-
-
 require('./bootstrap');
 
 window.Vue = require('vue');
 
-
-
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
-    el: '#app'
-    
+    el: '#app' 
 });
+
+
