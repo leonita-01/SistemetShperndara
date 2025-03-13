da053e04b4d0d91a801cdf6377d5afadedcb7fd4
SumejaV <sumiveseli@gmail.com>
2025-03-14 00:30:10 +0100

    Update product_details.blade.php

--- resources/views/frontend/product_details.blade.php
+++ resources/views/frontend/product_details.blade.php
@@ -22,12 +22,12 @@
             $availability = "in stock";
         }
     @endphp
-    <!-- Schema.org markup for Google+ -->
+   
     <meta itemprop="name" content="{{ $detailedProduct->meta_title }}">
     <meta itemprop="description" content="{{ $detailedProduct->meta_description }}">
     <meta itemprop="image" content="{{ uploaded_asset($detailedProduct->meta_img) }}">
 
-    <!-- Twitter Card data -->
+    
     <meta name="twitter:card" content="product">
     <meta name="twitter:site" content="@publisher_handle">
     <meta name="twitter:title" content="{{ $detailedProduct->meta_title }}">
@@ -37,7 +37,7 @@
     <meta name="twitter:data1" content="{{ single_price($detailedProduct->unit_price) }}">
     <meta name="twitter:label1" content="Price">
 
-    <!-- Open Graph data -->
+    
     <meta property="og:title" content="{{ $detailedProduct->meta_title }}" />
     <meta property="og:type" content="og:product" />
     <meta property="og:url" content="{{ route('product', $detailedProduct->slug) }}" />
@@ -60,12 +60,13 @@
         <div class="container">
             <div class="bg-white py-3">
                 <div class="row">
-                    <!-- Product Image Gallery -->
+                    
                     <div class="col-xl-5 col-lg-6 mb-4">
                         @include('frontend.product_details.image_gallery')
                     </div>
 
-                    <!-- Product Details -->
+                    
+
                     <div class="col-xl-7 col-lg-6">
                         @include('frontend.product_details.details')
                     </div>
@@ -77,43 +78,45 @@
     <section class="mb-4">
         <div class="container">
             @if ($detailedProduct->auction_product)
-                <!-- Reviews & Ratings -->
+                
+            
                 @include('frontend.product_details.review_section')
                 
-                <!-- Description, Video, Downloads -->
+              
+                
                 @include('frontend.product_details.description')
                 
-                <!-- Product Query -->
+               
                 @include('frontend.product_details.product_queries')
             @else
                 <div class="row gutters-16">
-                    <!-- Left side -->
+                 
                     <div class="col-lg-3">
-                        <!-- Seller Info -->
+                        
                         @include('frontend.product_details.seller_info')
 
-                        <!-- Top Selling Products -->
+                       
                        <div class="d-none d-lg-block">
                             @include('frontend.product_details.top_selling_products')
                        </div>
                     </div>
 
-                    <!-- Right side -->
+                    
                     <div class="col-lg-9">
                         
-                        <!-- Reviews & Ratings -->
+                       
                         @include('frontend.product_details.review_section')
 
-                        <!-- Description, Video, Downloads -->
+                        
                         @include('frontend.product_details.description')
                         
-                        <!-- Frequently Bought products -->
+                        
                         @include('frontend.product_details.frequently_bought_products')
 
-                        <!-- Product Query -->
+                        
                         @include('frontend.product_details.product_queries')
                         
-                        <!-- Top Selling Products -->
+                       
                         <div class="d-lg-none">
                              @include('frontend.product_details.top_selling_products')
                         </div>
@@ -144,7 +147,7 @@
 @endsection
 
 @section('modal')
-    <!-- Image Modal -->
+    
     <div class="modal fade" id="image_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-dialog-zoom product-modal" id="modal-size" role="document">
@@ -166,7 +169,8 @@
         </div>
     </div>
 
-    <!-- Chat Modal -->
+
+
     <div class="modal fade" id="chat_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-dialog-zoom product-modal" id="modal-size" role="document">
@@ -177,6 +181,7 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
+
                 <form class="" action="{{ route('conversations.store') }}" method="POST"
                     enctype="multipart/form-data">
                     @csrf
@@ -187,11 +192,13 @@
                                 value="{{ $detailedProduct->name }}" placeholder="{{ translate('Product Name') }}"
                                 required>
                         </div>
+
                         <div class="form-group">
                             <textarea class="form-control rounded-0" rows="8" name="message" required
                                 placeholder="{{ translate('Your Question') }}">{{ route('product', $detailedProduct->slug) }}</textarea>
                         </div>
                     </div>
+
                     <div class="modal-footer">
                         <button type="button" class="btn btn-outline-primary fw-600 rounded-0"
                             data-dismiss="modal">{{ translate('Cancel') }}</button>
@@ -202,7 +209,7 @@
         </div>
     </div>
 
-    <!-- Bid Modal -->
+    
     @if($detailedProduct->auction_product == 1)
         @php 
             $highest_bid = $detailedProduct->bids->max('amount');
@@ -238,8 +245,10 @@
             </div>
         </div>
     @endif
+
     
-    <!-- Product Review Modal -->
+   
+
     <div class="modal fade" id="product-review-modal">
         <div class="modal-dialog">
             <div class="modal-content" id="product-review-modal-content">
@@ -248,10 +257,11 @@
         </div>
     </div>
 
-    <!-- Size chart show Modal -->
+    
     @include('modals.size_chart_show_modal')
 
-    <!-- Product Warranty Modal -->
+    
+    
     <div class="modal fade" id="warranty-note-modal">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
@@ -268,7 +278,7 @@
         </div>
     </div>
 
-    <!-- Product Refund Modal -->
+   
     <div class="modal fade" id="refund-note-modal">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
@@ -304,23 +314,9 @@
                 AIZ.plugins.notify('danger', '{{ translate('Oops, unable to copy') }}');
             }
             $temp.remove();
-            // if (document.selection) {
-            //     var range = document.body.createTextRange();
-            //     range.moveToElementText(document.getElementById(containerid));
-            //     range.select().createTextRange();
-            //     document.execCommand("Copy");
-
-            // } else if (window.getSelection) {
-            //     var range = document.createRange();
-            //     document.getElementById(containerid).style.display = "block";
-            //     range.selectNode(document.getElementById(containerid));
-            //     window.getSelection().addRange(range);
-            //     document.execCommand("Copy");
-            //     document.getElementById(containerid).style.display = "none";
-
-            // }
-            // AIZ.plugins.notify('success', 'Copied');
+           
         }
+
 
         function show_chat_modal() {
             @if (Auth::check())
@@ -330,7 +326,6 @@
             @endif
         }
 
-        // Pagination using ajax
         $(window).on('hashchange', function() {
             if(window.history.pushState) {
                 window.history.pushState('', '/', window.location.pathname);
@@ -365,7 +360,7 @@
                 alert('Something went worng! Data could not be loaded.');
             });
         }
-        // Pagination end
+       
 
         function showImage(photo) {
             $('#image_modal img').attr('src', photo);
@@ -426,3 +421,5 @@
         }
     </script>
 @endsection
+
+
