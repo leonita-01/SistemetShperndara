b12cd22ecd2298410f52935b8d933cf1a6bb3fe7
SumejaV <sumiveseli@gmail.com>
2025-03-14 00:48:29 +0100

    Update details.blade.php

--- resources/views/frontend/product_details/details.blade.php
+++ resources/views/frontend/product_details/details.blade.php
@@ -1,11 +1,11 @@
 <div class="text-left">
-    <!-- Product Name -->
+ 
     <h2 class="mb-4 fs-16 fw-700 text-dark">
         {{ $detailedProduct->getTranslation('name') }}
     </h2>
 
     <div class="row align-items-center mb-3">
-        <!-- Review -->
+       
         @if ($detailedProduct->auction_product != 1)
             <div class="col-12">
                 @php
@@ -19,14 +19,14 @@
                     {{ translate('reviews') }})</span>
             </div>
         @endif
-        <!-- Estimate Shipping Time -->
+       
         @if ($detailedProduct->est_shipping_days)
             <div class="col-auto fs-14 mt-1">
                 <small class="mr-1 opacity-50 fs-14">{{ translate('Estimate Shipping Time') }}:</small>
                 <span class="fw-500">{{ $detailedProduct->est_shipping_days }} {{ translate('Days') }}</span>
             </div>
         @endif
-        <!-- In stock -->
+       
         @if ($detailedProduct->digital == 1)
             <div class="col-12 mt-1">
                 <span class="badge badge-md badge-inline badge-pill badge-success">{{ translate('In stock') }}</span>
@@ -35,7 +35,7 @@
     </div>
     <div class="row align-items-center">
         @if(get_setting('product_query_activation') == 1)
-            <!-- Ask about this product -->
+            
             <div class="col-xl-3 col-lg-4 col-md-3 col-sm-4 mb-3">
                 <a href="javascript:void();" onclick="goToView('product_query')" class="text-primary fs-14 fw-600 d-flex">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32">
@@ -60,13 +60,13 @@
         <div class="col mb-3">
             @if ($detailedProduct->auction_product != 1)
                 <div class="d-flex">
-                    <!-- Add to wishlist button -->
+                    
                     <a href="javascript:void(0)" onclick="addToWishList({{ $detailedProduct->id }})"
                         class="mr-3 fs-14 text-dark opacity-60 has-transitiuon hov-opacity-100">
                         <i class="la la-heart-o mr-1"></i>
                         {{ translate('Add to Wishlist') }}
                     </a>
-                    <!-- Add to compare button -->
+                   
                     <a href="javascript:void(0)" onclick="addToCompare({{ $detailedProduct->id }})"
                         class="fs-14 text-dark opacity-60 has-transitiuon hov-opacity-100">
                         <i class="las la-sync mr-1"></i>
@@ -78,7 +78,7 @@
     </div>
 
 
-    <!-- Brand Logo & Name -->
+    
     @if ($detailedProduct->brand != null)
         <div class="d-flex flex-wrap align-items-center mb-3">
             <span class="text-secondary fs-14 fw-400 mr-4 w-80px">{{ translate('Brand') }}</span><br>
@@ -86,6 +86,7 @@
                 class="text-reset hov-text-primary fs-14 fw-700">{{ $detailedProduct->brand->name }}</a>
         </div>
     @endif
+
 
     {{-- Warranty --}}
     @if ($detailedProduct->has_warranty == 1 && $detailedProduct->warranty_id != null)
@@ -105,10 +106,10 @@
         </div>
     @endif
 
-    <!-- Seller Info -->
+    
     <div class="d-flex flex-wrap align-items-center">
         <div class="d-flex align-items-center mr-4">
-            <!-- Shop Name -->
+           
             @if ($detailedProduct->added_by == 'seller' && get_setting('vendor_system_activation') == 1)
                 <span class="text-secondary fs-14 fw-400 mr-4 w-80px">{{ translate('Sold by') }}</span>
                 <a href="{{ route('shop.visit', $detailedProduct->user->shop->slug) }}"
@@ -117,7 +118,7 @@
                 <p class="mb-0 fs-14 fw-700">{{ translate('Inhouse product') }}</p>
             @endif
         </div>
-        <!-- Messase to seller -->
+        
         @if (get_setting('conversation_system') == 1)
             <div class="">
                 <button class="btn btn-sm btn-soft-secondary-base btn-outline-secondary-base hov-svg-white hov-text-white rounded-4"
@@ -144,7 +145,7 @@
                 </button>
             </div>
         @endif
-        <!-- Size guide -->
+       
         @php
             $sizeChartId = ($detailedProduct->main_category && $detailedProduct->main_category->sizeChart) ? $detailedProduct->main_category->sizeChart->id : 0;
             $sizeChartName = ($detailedProduct->main_category && $detailedProduct->main_category->sizeChart) ? $detailedProduct->main_category->sizeChart->name : null;
@@ -158,7 +159,6 @@
 
     <hr>
 
-    <!-- For auction product -->
     @if ($detailedProduct->auction_product)
         <div class="row no-gutters mb-3">
             <div class="col-sm-2">
@@ -218,9 +218,9 @@
             </div>
         </div>
     @else
-        <!-- Without auction product -->
+       
         @if ($detailedProduct->wholesale_product == 1)
-            <!-- Wholesale -->
+            
             <table class="table mb-3">
                 <thead>
                     <tr>
@@ -230,6 +230,7 @@
                     </tr>
                 </thead>
                 <tbody>
+
                     @foreach ($detailedProduct->stocks->first()->wholesalePrices as $wholesalePrice)
                         <tr>
                             <td>{{ $wholesalePrice->min_qty }}</td>
@@ -237,10 +238,11 @@
                             <td>{{ single_price($wholesalePrice->price) }}</td>
                         </tr>
                     @endforeach
+
                 </tbody>
             </table>
         @else
-            <!-- Without Wholesale -->
+           
             @if (home_price($detailedProduct) != home_discounted_price($detailedProduct))
                 <div class="row no-gutters mb-3">
                     <div class="col-sm-2">
@@ -248,24 +250,24 @@
                     </div>
                     <div class="col-sm-10">
                         <div class="d-flex align-items-center">
-                            <!-- Discount Price -->
+                           
                             <strong class="fs-16 fw-700 text-primary">
                                 {{ home_discounted_price($detailedProduct) }}
                             </strong>
-                            <!-- Home Price -->
+                            
                             <del class="fs-14 opacity-60 ml-2">
                                 {{ home_price($detailedProduct) }}
                             </del>
-                            <!-- Unit -->
+                           
                             @if ($detailedProduct->unit != null)
                                 <span class="opacity-70 ml-1">/{{ $detailedProduct->getTranslation('unit') }}</span>
                             @endif
-                            <!-- Discount percentage -->
+                            
                             @if (discount_in_percentage($detailedProduct) > 0)
                                 <span class="bg-primary ml-2 fs-11 fw-700 text-white w-35px text-center p-1"
                                     style="padding-top:2px;padding-bottom:2px;">-{{ discount_in_percentage($detailedProduct) }}%</span>
                             @endif
-                            <!-- Club Point -->
+                           
                             @if (addon_is_activated('club_point') && $detailedProduct->earn_point > 0)
                                 <div class="ml-2 bg-secondary-base d-flex justify-content-center align-items-center px-3 py-1"
                                     style="width: fit-content;">
@@ -352,7 +354,7 @@
             <input type="hidden" name="id" value="{{ $detailedProduct->id }}">
 
             @if ($detailedProduct->digital == 0)
-                <!-- Choice Options -->
+                
                 @if ($detailedProduct->choice_options != null)
                     @foreach (json_decode($detailedProduct->choice_options) as $key => $choice)
                         <div class="row no-gutters mb-3">
@@ -380,7 +382,6 @@
                     @endforeach
                 @endif
 
-                <!-- Color Options -->
                 @if ($detailedProduct->colors != null && count(json_decode($detailedProduct->colors)) > 0)
                     <div class="row no-gutters mb-3">
                         <div class="col-sm-2">
@@ -406,7 +407,7 @@
                     </div>
                 @endif
 
-                <!-- Quantity + Add to cart -->
+                
                 <div class="row no-gutters mb-3">
                     <div class="col-sm-2">
                         <div class="text-secondary fs-14 fw-400 mt-2">{{ translate('Quantity') }}</div>
@@ -445,11 +446,11 @@
                     </div>
                 </div>
             @else
-                <!-- Quantity -->
+                
                 <input type="hidden" name="quantity" value="1">
             @endif
 
-            <!-- Total Price -->
+            
             <div class="row no-gutters pb-3 d-none" id="chosen_price_div">
                 <div class="col-sm-2">
                     <div class="text-secondary fs-14 fw-400 mt-1">{{ translate('Total Price') }}</div>
@@ -491,7 +492,7 @@
             </div>
         @endif
     @else
-        <!-- Add to cart & Buy now Buttons -->
+      
         <div class="mt-3">
             @if ($detailedProduct->digital == 0)
                 @if (((get_setting('product_external_link_for_seller') == 1) && ($detailedProduct->added_by == "seller") && ($detailedProduct->external_link != null)) ||
@@ -554,7 +555,7 @@
             </div>
         </div>
 
-        <!-- Refund -->
+        <
         @php
             $refund_sticker = get_setting('refund_sticker');
         @endphp
@@ -589,7 +590,7 @@
             </div>
         @endif
 
-        <!-- Seller Guarantees -->
+       
         @if ($detailedProduct->digital == 1)
             @if ($detailedProduct->added_by == 'seller')
                 <div class="row no-gutters mt-3">
@@ -608,7 +609,7 @@
         @endif
     @endif
 
-    <!-- Share -->
+   
     <div class="row no-gutters mt-4">
         <div class="col-sm-2">
             <div class="text-secondary fs-14 fw-400 mt-2">{{ translate('Share') }}</div>
@@ -618,3 +619,4 @@
         </div>
     </div>
 </div>
+
